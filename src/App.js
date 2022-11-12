9781b2e39fd40b66603792f175e998335fe45038
sasalilime <salimtison@gmail.com>
2022-11-12 20:25:46 +0100

    done a todo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import TodoList from './components/TodoList';
 import {todosState} from './recoil';
 
 function App() {
-    const todos = useRecoilValue(todosState);
-
-    console.log(todos);
     return (
         <div className='d-flex flex-row justify-content-center align-items-center p-20'>
             <div className='card container p-20'>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,14 +4,24 @@ import {todosState} from '../recoil';
 function TodoItem({todo}) {
     const setTodosState = useSetRecoilState(todosState);
 
+    const handleClickDone = () => {
+        setTodosState((oldTodosState) =>
+            oldTodosState.map((oldTodo) => (oldTodo._id === todo._id ? {...todo, done: !todo.done} : oldTodo))
+        );
+    };
+
     const handleClickDelete = () => {
         setTodosState((oldTodosState) => oldTodosState.filter((oldTodo) => oldTodo._id !== todo._id));
     };
 
     return (
         <li className='d-flex align-items-center mb-20'>
-            <span className='flex-fill mr-15'>{todo.content}</span>
-            <button className='btn btn-primary mr-15'>Valider</button>
+            <span style={{textDecoration: todo.done && 'line-through'}} className='flex-fill mr-15'>
+                {todo.content}
+            </span>
+            <button onClick={handleClickDone} className='btn btn-primary mr-15'>
+                {todo.done ? 'Annuler' : 'Fait'}
+            </button>
             <button onClick={handleClickDelete} className='btn btn-danger mr-15'>
                 Supprimer
             </button>

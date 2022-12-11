a0f3f00b7b439274fe3868333f229d2de1f47342
sasalilime <salimtison@gmail.com>
2022-12-11 23:05:09 +0100

    Todos Ok

--- src/recoil/index.js
+++ src/recoil/index.js
@@ -1,54 +1,53 @@
-import { atom, selector, selectorFamily } from 'recoil'
-import { getTodosRequest } from '../api'
+import {atom, selector, selectorFamily} from 'recoil';
+import {getTodoRequest, getTodosRequest} from '../api';
 
 export const todosState = atom({
-  key: 'todosState',
-  default: getTodosRequest(),
-})
+    key: 'todosState',
+    default: getTodosRequest(),
+});
 
 export const filterState = atom({
-  key: 'filterState',
-  default: 'all',
-})
+    key: 'filterState',
+    default: 'all',
+});
 
 export const selectFilteredTodos = selector({
-  key: 'selectFilteredTodos',
-  get: ({ get }) => {
-    const filter = get(filterState)
-    const todos = get(todosState)
-    switch (filter) {
-      case 'done':
-        return todos.filter((t) => t.done)
-      case 'ongoing':
-        return todos.filter((t) => !t.done)
-      default:
-        return todos
-    }
-  },
-})
+    key: 'selectFilteredTodos',
+    get: ({get}) => {
+        const filter = get(filterState);
+        const todos = get(todosState);
+        switch (filter) {
+            case 'done':
+                return todos.filter((t) => t.done);
+            case 'ongoing':
+                return todos.filter((t) => !t.done);
+            default:
+                return todos;
+        }
+    },
+});
 
 export const selectTodosStats = selector({
-  key: 'selectTodosStats',
-  get: ({ get }) => {
-    const todos = get(todosState)
-    const total = todos.length
-    const totalOnGoing = todos.filter((t) => !t.done).length
-    const totalDone = todos.filter((t) => t.done).length
-    return {
-      total,
-      totalOnGoing,
-      totalDone,
-      totalDonePercent: total > 0 ? Math.floor((totalDone / total) * 100) : 0,
-    }
-  },
-})
+    key: 'selectTodosStats',
+    get: ({get}) => {
+        const todos = get(todosState);
+        const total = todos.length;
+        const totalOnGoing = todos.filter((t) => !t.done).length;
+        const totalDone = todos.filter((t) => t.done).length;
+        return {
+            total,
+            totalOnGoing,
+            totalDone,
+            totalDonePercent: total > 0 ? Math.floor((totalDone / total) * 100) : 0,
+        };
+    },
+});
 
 export const selectTodoDetails = selectorFamily({
-  key: 'selectTodoDetails',
-  get:
-    (_id) =>
-      ({ get }) => {
-        const todos = get(todosState)
-        return _id ? todos.find((t) => t._id === _id) : null
-      },
-})
+    key: 'selectTodoDetails',
+    get:
+        (_id) =>
+        async ({get}) => {
+            return _id && getTodoRequest(_id);
+        },
+});
